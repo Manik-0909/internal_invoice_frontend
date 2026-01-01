95ab4bf78e1f96bd6ed7c6ca8118c2ed2c817c42
Abhi <abhishekchoudhary3139@gmail.com>
2026-01-01 22:40:22 +0530

    Update packages versions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n/config';
 import ComingSoon from './pages/ComingSoon';
 import './styles/index.css';
-
 const App: React.FC = () => {
   return (
     <I18nextProvider i18n={i18n}>
