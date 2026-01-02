c45c546f7d782b0fd04bb3ee597b71af13b63c4e
Manik Gupta <manikgupta254@gmail.com>
2026-01-03 01:09:54 +0530

    Added content for screen 5

---
+++ src/components/progressBar/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ProgressBar = ({ bgGradientColor, progress }) => {
+  const containerStyles = {
+    height: 5,
+    width: '100%',
+    backgroundColor: '#f1f1f1',
+    borderRadius: 50,
+  };
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${progress}%`,
+    backgroundColor: bgGradientColor,
+    borderRadius: 'inherit',
+    textAlign: 'right',
+  };
+
+  const labelStyles = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold',
+  };
+
+  return (
+    <div className="progress-bar" style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{progress}%</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
